b31a7f0c548da3cf2843b5820dfbf3b1a2f006b2
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2018-06-21 16:32:54 -0300

    Versão 0.0.7

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let doConnect = () => {
         conn.events('json', 'all')
 
         conn.on('esl::event::CHANNEL_HANGUP_COMPLETE::*', function(e) {
-            if(e.getHeader('Call-Direction') == 'inbound'){
+            if(e.getHeader('Call-Direction') == 'inbound' && (e.getHeader('Caller-Network-Addr') == '200.225.81.77' || e.getHeader('Caller-Network-Addr') == '18.217.251.102')){
 
                 if(e.getHeader('variable_sip_h_P-CostCenter')){
                     insert = [e.getHeader('Unique-ID'), e.getHeader('variable_sip_contact_user'), '']
